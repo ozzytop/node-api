a4b81c8ac3a34f8ab0caefa0dba5980ca82da8a7
Matias Penas <matiaspenas@Matiass-MacBook-Pro-2.local>
2021-06-13 18:59:02 -0300

    Adding error validations

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -13,8 +13,8 @@ exports.getBootcamps = async (req, res, next) => {
       count: bootcamps.length,
       data: bootcamps
     });
-  } catch (error) {
-    res.status(400).json({ success:false });
+  } catch (err) {
+    next(err);
   }
   
 }
@@ -47,17 +47,11 @@ exports.createBootcamp = async (req, res, next) => {
 
     const bootcamp = await Bootcamp.create(req.body);
   
-    res.status(201).json({
-      success:true,
-      data: bootcamp
-    });
+    res.status(201).json({ success:true, data: bootcamp });
     
-  } catch (error) {
+  } catch (err) {
 
-    res.status(400).json({
-      success:false
-    });
-
+    next(err);
   }
 
 
@@ -75,12 +69,12 @@ exports.updateBootcamp = async (req, res, next) => {
     });
     
     if(!bootcamp){
-      return res.status(400).json({ success:false });
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
     
     res.status(200).json({ success: true, data: bootcamp });    
-  } catch (error) {
-    res.status(400).json({ success:false });
+  } catch (err) {
+    next(err);
   }
 
 }
@@ -94,13 +88,13 @@ exports.deleteBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
     
     if(!bootcamp){
-      return res.status(400).json({ success:false });
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
     
     res.status(200).json({ success: true, data: {} });
 
-  } catch (error) {
-    res.status(400).json({ success:false });
+  } catch (err) {
+    next(err);
   }
 
 }
--- middleware/error.js
+++ middleware/error.js
@@ -7,7 +7,7 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   //Log To console
-  console.log(err.stack.red);
+  console.log(err);
   
   // Mongoose bad ObjectId
   if(err.name === 'CastError'){
@@ -15,11 +15,24 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 404);
   }
   
+  // Mongoose duplicated key
+  if(err.code === 11000){
+    const message = `Duplicated field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+  
+  // Mongoose validation error
+  if(err.name === 'ValidationError'){
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+  
+
   res.status( error.statusCode || 500).json({
     success: false,
     error: error.message || 'Server Error'
   });
 
 }
-
+ 
 module.exports = errorHandler;
